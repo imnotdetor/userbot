c7e656316c477795c7e48fc50176dc08c5b91ff7
imnotdetor <avanishpal73092@gmail.com>
2026-01-28 18:44:40 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")
-MONGO_URL = os.getenv("MONGO_URL")
-
-OWNER_ID = int(os.getenv("OWNER_ID"))
