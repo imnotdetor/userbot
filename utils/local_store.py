b041e700371bb6dc1cfcea12c25b5b083ff88bf3
imnotdetor <avanishpal73092@gmail.com>
2026-01-30 09:50:32 +0530

    Update local_store.py

--- utils/local_store.py
+++ utils/local_store.py
@@ -1,20 +1,38 @@
 import json
 import os
-from threading import Lock
 
-_lock = Lock()
+FILE = "utils/antipm_data.json"
 
-def load_json(path, default):
-    if not os.path.exists(path):
-        return default
-    try:
-        with open(path, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except:
-        return default
+def _load():
+    if not os.path.exists(FILE):
+        return {"state": {"enabled": True, "silent": False}, "users": {}}
+    with open(FILE, "r") as f:
+        return json.load(f)
 
-def save_json(path, data):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    with _lock:
-        with open(path, "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=2)
+def _save(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=2)
+
+def get_state():
+    return _load()["state"]
+
+def set_state(k, v):
+    d = _load()
+    d["state"][k] = v
+    _save(d)
+
+def get_user(uid):
+    return _load()["users"].get(str(uid))
+
+def save_user(uid, data):
+    d = _load()
+    d["users"][str(uid)] = data
+    _save(d)
+
+def reset_user(uid):
+    d = _load()
+    d["users"].pop(str(uid), None)
+    _save(d)
+
+def list_users():
+    return _load()["users"]
