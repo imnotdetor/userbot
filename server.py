60d45b3063fa1d0d8da55d57ad3e5b8e4fabe839
imnotdetor <avanishpal73092@gmail.com>
2026-01-31 13:50:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -19,4 +19,5 @@ def capture():
     return jsonify({"status": "logged"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
